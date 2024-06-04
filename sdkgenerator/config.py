8a45433601e18c4477ee204043107a24842eb028
Mounir <polomnr1@gmail.com>
2024-06-05 00:40:46 +0100

    add: get configuration from yaml file

--- sdkgenerator/config.py
+++ sdkgenerator/config.py
@@ -1,41 +1,10 @@
+import yaml
 from sdkgenerator.types import Step, Agent
 
-TEMPERATURE: dict[Step, float] = {
-    "types": 0.1,
-    "initial_code": 0.2,
-    "feedback": 0.2,
-    "final_code": 0.1,
-}
+with open("generator.config.yaml", "r") as file:
+    config = yaml.safe_load(file)
 
-AGENT: dict[Step, Agent] = {
-    "types": {
-        "model": "gpt-4-32k-0314",
-        "custom": False,
-    },
-    "initial_code": {
-        "model": "ft:gpt-3.5-turbo-0125:eden-ai::9UxoYDO3",
-        "custom": True,
-    },
-    "feedback": {
-        "model": "gpt-4-32k-0314",
-        "custom": False,
-    },
-    "final_code": {
-        "model": "ft:gpt-3.5-turbo-0125:eden-ai::9UxoYDO3",
-        "custom": True,
-    },
-}
-
-MAX_PROMPT_LENGTH: dict[Step, int] = {
-    "types": 8192,
-    "initial_code": 8192,
-    "feedback": 8192,
-    "final_code": 8192,
-}
-
-MAX_TOKENS: dict[Step, int] = {
-    "types": 4096,
-    "initial_code": 4096,
-    "feedback": 4096,
-    "final_code": 4096,
-}
+TEMPERATURE: dict[Step, float] = config["TEMPERATURE"]
+AGENT: dict[Step, Agent] = config["AGENT"]
+MAX_PROMPT_LENGTH: dict[Step, int] = config["MAX_PROMPT_LENGTH"]
+MAX_TOKENS: dict[Step, int] = config["MAX_TOKENS"]
